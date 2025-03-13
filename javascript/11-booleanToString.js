4df557189f86177747e8993ede7f1855eaae47f1
FungkiAndika <fungki4444@gmail.com>
2025-03-13 16:47:12 +0700

    13/03

---
+++ javascript/11-booleanToString.js
@@ -0,0 +1,11 @@
+// function booleanToString(b){
+//     if (b == Boolean | b == true) {
+//         return b.toString()
+//     } else {
+//         return b = "false"
+//     }
+// }
+
+// ONE LINE SCRIPT
+const booleanToString = (b) => b == Boolean | b == true ? b.toString() : b = "false";
+console.log(booleanToString());
