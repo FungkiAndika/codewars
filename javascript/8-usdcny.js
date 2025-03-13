7d07ae5ef6a897444da4c729e5b7571f9eec25df
FungkiAndika <fungki4444@gmail.com>
2025-03-13 07:40:52 +0700

    13/03

---
+++ javascript/8-usdcny.js
@@ -0,0 +1,21 @@
+// function usdcny(usd) {
+//     let y = usd * 6.75
+//     // if (y > 1000, y < 100) {
+//     //     return y.toFixed(2)+" Chinese Yuan"
+//     // }
+//     // if (y % 2 === 0 , y % 3 === 0) {
+//     //     return y.toFixed(2)+" Chinese Yuan"
+//     // }
+//     // else {
+//     //     return y.toFixed(2)+" Chinese Yuan"
+//     // } 
+//     return y.toFixed(2)+" Chinese Yuan"
+// }
+
+// ONE LINE FUNCTION
+// usdcny = $ => `${($*6.75).toFixed(2)} Chinese Yuan`
+const usdcny = (usd) => (usd*6.75).toFixed(2)+" Chinese Yuan"
+
+console.log(usdcny(15));
+console.log(usdcny(6758));
+console.log(usdcny(1000));
