c660f164111a19ad64d52ef0d68347ae26896cf1
FungkiAndika <fungki4444@gmail.com>
2025-03-14 14:45:35 +0700

    14/03

---
+++ javascript/15-friend.js
@@ -0,0 +1,7 @@
+// function friend(friends){
+//     return friends.filter(s => s.length == 4)
+// }
+
+//  ONE LINE
+const friend = (friends) => friends.filter(s => s.length == 4)
+console.log(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]));
