a44958ca21c1830d4f16964bc752775abeca59ea
FungkiAndika <fungki4444@gmail.com>
2025-03-14 09:21:53 +0700

    14/03

--- javascript/12-filter_list.js
+++ javascript/12-filter_list.js
@@ -4,4 +4,6 @@
 
 // ONE LINE SCRIPT
 const filter_list = l => l.filter(a => typeof a === 'number')
+// ALTER :
+// const filter_list = l => l.filter(Number.isInteger)
 console.log(filter_list([1,2,'aasf','1','123',123]));
