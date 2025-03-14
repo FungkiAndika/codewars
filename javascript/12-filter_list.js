0a18ccea8c8461f44b058fae71487447077133a8
FungkiAndika <fungki4444@gmail.com>
2025-03-14 09:16:31 +0700

    14/03

---
+++ javascript/12-filter_list.js
@@ -0,0 +1,7 @@
+// function filter_list(l) {
+//     return l.filter(a => typeof a === 'number')
+// }
+
+// ONE LINE SCRIPT
+const filter_list = l => l.filter(a => typeof a === 'number')
+console.log(filter_list([1,2,'aasf','1','123',123]));
