549168f77b93db408631e0b469d00477a84a2502
FungkiAndika <fungki4444@gmail.com>
2025-03-14 09:55:24 +0700

    14/03

---
+++ javascript/13-disemvowel.js
@@ -0,0 +1,7 @@
+function disemvowel(str) {
+    return str.split("").filter(s => s !="a" & s !="u" & s !="i" & s !="e" & s !="o" & s !="A" & s !="U" & s !="I" & s !="E" & s !="O").join("")
+}
+
+// ALTER :
+// const disemvowel = str => str.replace(/[auieo]/gi,'')
+console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read"));
