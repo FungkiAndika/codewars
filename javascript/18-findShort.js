b907721656e18a651fa22fd8d8772b7543c3677f
FungkiAndika <fungki4444@gmail.com>
2025-03-15 14:31:45 +0700

    15/03

--- javascript/18-findShort.js
+++ javascript/18-findShort.js
@@ -2,6 +2,8 @@
 //     return Math.min(...s.split(' ').filter(a => a.length > 0).map(n => n.length))
 // }
 
-// ONE LINE
-const findShort = (s) => Math.min(...s.split(' ').filter(a => a.length > 0).map(n => n.length))
-console.log(findShort("hda doah  uas paa w  asjasfj asoj fa afjafasj"));
+// ONE LINE (no include " ")/( higher than 0 )
+// const findShort = (s) => Math.min(...s.split(' ').filter(a => a.length > 0).map(n => n.length))
+// ALTER (include " ") :
+const findShort = s => Math.min(...s.split(' ').map(a => a.length ))
+console.log(findShort("hda doah uas paa w asjasfj asoj fa afjafasj"));
