7dccaa8d05eec845c2395daea3fe599b45b722b5
FungkiAndika <fungki4444@gmail.com>
2025-03-15 13:15:41 +0700

    15/03

--- javascript/16-divisors.js
+++ javascript/16-divisors.js
@@ -1,5 +1,5 @@
 function divisors(integer) {
-    const y = Array.from({length: integer/2}, (_, index) => index +1)
-    return y.filter((a) => integer % a === 0).filter(b => b > 1).length == 0 ? integer + " is prime": y.filter((a) => integer % a === 0).filter(b => b > 1)
+    const y = Array.from({length: integer/2}, (_, index) => index +1).filter((a) => integer % a === 0).filter(b => b > 1)
+    return y.length == 0 ? integer + " is prime": y
 }
 console.log(divisors(8));
