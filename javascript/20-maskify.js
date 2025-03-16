66157dbe0054740c11a89edb9f446ca987bef404
FungkiAndika <fungki4444@gmail.com>
2025-03-16 19:08:59 +0700

    16/03

--- javascript/20-maskify.js
+++ javascript/20-maskify.js
@@ -4,4 +4,6 @@
 
 // ONE LINE
 const maskify = cc => cc.length < 5 ? cc : cc.slice(0, cc.length-4).split('').map(a => "#").join('')+cc.slice(cc.length-4)
+// ALTER :
+// const maskify = cc => cc.slice(-4).padStart(cc.length,'#')
 console.log(maskify("qwerty"));
