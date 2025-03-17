1a4646201fcc671e0083d7e287b6c2ea57c5ef4f
FungkiAndika <fungki4444@gmail.com>
2025-03-17 19:05:35 +0700

    17/03

---
+++ python/1-even_or_odd.py
@@ -0,0 +1,11 @@
+# def even_or_odd(number):
+#     if number % 2 == 0 :
+#         number = 'Even'
+#     else :
+#         number = 'Odd'
+#     return number
+
+# ALTER
+def even_or_odd(number):
+    return 'Even' if number % 2 == 0 else 'Odd'
+print(even_or_odd(4))
