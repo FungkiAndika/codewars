46d57969af6012bb595d6465572ad39f59ae9d12
FungkiAndika <fungki4444@gmail.com>
2025-03-18 14:15:04 +0700

    18/03

---
+++ python/4-greet.py
@@ -0,0 +1,6 @@
+# def greet(name):
+#     return "Hello, "+name+" how are you doing today?"
+
+# LAMBDA FUNCTION
+greet = lambda name : "Hello, "+name+" how are you doing today?"
+print(greet("Loser"))
