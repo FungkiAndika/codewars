d6299d8f2a6918a1c663da4e21134dd4558eacf2
FungkiAndika <fungki4444@gmail.com>
2025-03-18 17:14:07 +0700

    18/03

---
+++ javascript/24-twoSum.js
@@ -0,0 +1,13 @@
+// function twoSum(numbers, target) {
+//     for (let i = 0; i < numbers.length -1; i++) {
+//         for (let j = i + 1; j < numbers.length; j++) {
+//             if (numbers[i] + numbers[j] === target) {
+//                 return [i,j]
+//             }
+//         }
+//     }
+// }
+
+// ALTER
+const twoSum = (a, n) => {for (let i = 0; i < a.length -1; i++) {for (let j = i + 1; j < a.length; j++) {if (a[i] + a[j] === n) return [i,j]}}}
+console.log(twoSum([1,2,3],4));
