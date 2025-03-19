ccfde2a754c0ab87153c5a2cb5ffbfc247fb7d93
FungkiAndika <fungki4444@gmail.com>
2025-03-19 09:38:32 +0700

    19/03

---
+++ javascript/25-nbYear.js
@@ -0,0 +1,12 @@
+// function nbYear(p0, percent, aug, p) {  
+//     let years = 0
+//     while (p0 < p) {
+//         p0 += Math.floor(p0*(percent/100)+aug)
+//         years++
+//     }
+//     return years
+// }
+
+// ARROW FUNCTION
+const nbYear = (p0, percent, aug, p) => {let years = 0 ; while (p0 < p) {p0 += Math.floor(p0*(percent/100)+aug); years++}; years}
+console.log(nbYear(1500, 5, 100, 5000));
