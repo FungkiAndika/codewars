f50ec62385730561e708ede785e4c31f59aa416c
FungkiAndika <fungki4444@gmail.com>
2025-03-20 17:46:44 +0700

    20/03

---
+++ javascript/26-sumTwoSmallestNumbers.js
@@ -0,0 +1,10 @@
+// function sumTwoSmallestNumbers(numbers) {
+//     return numbers.sort((a,b)=> a-b)[0]+numbers.sort((a,b)=> a-b)[1]
+// }
+
+// ALTER :
+function sumTwoSmallestNumbers(numbers) {
+    var [a,b] = numbers.sort((a,b)=>a-b)
+    return a+b
+}
+console.log(sumTwoSmallestNumbers([12, 5, 8, 19, 22]));
