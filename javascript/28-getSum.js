6143dd2b43f24612c6b5d1f614e9b22e1202488b
FungkiAndika <fungki4444@gmail.com>
2025-03-21 09:31:54 +0700

    21/03

---
+++ javascript/28-getSum.js
@@ -0,0 +1,15 @@
+// function getSum(a,b) {
+// //      create array                  which length                      fill array with            sum all
+// //       with value →                   of array                    min number to max number       in array
+// //           ↓        {                     ↓                   }  {             ↓           } {        ↓         }
+//     return Array.from({length: Math.max(a,b) - Math.min(a,b) + 1}, (_,i) => Math.min(a,b) + i).reduce((n,i) => n+i)
+// }
+// ALTER :
+function getSum(a,b) {
+    // (count of element) * (a+b) / 2
+    // 
+    // to positive number 
+    //            ↓
+    return (Math.abs(a-b) + 1) * (a+b)/2
+}
+console.log(getSum(8,4));
