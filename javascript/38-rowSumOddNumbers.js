4aaf36868584fa8c622bed4d8d8d7777dda9e845
FungkiAndika <fungki4444@gmail.com>
2025-03-23 22:32:48 +0700

    23/03

---
+++ javascript/38-rowSumOddNumbers.js
@@ -0,0 +1,17 @@
+/*
+                1
+            3     5
+        7     9    11
+    13    15    17    19
+21    23    25    27    29
+...
+
+    formula :  Sn​=n3
+*/
+
+// function rowSumOddNumbers(n) {
+//     return n**3
+// }
+rowSumOddNumbers = n => n**3
+console.log(rowSumOddNumbers(1), 1);
+console.log(rowSumOddNumbers(42), 74088);
