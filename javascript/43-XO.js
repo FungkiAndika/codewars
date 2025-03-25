ba223c5300c4d1ee490ddd556a9db17ad8d3d604
FungkiAndika <fungki4444@gmail.com>
2025-03-25 15:49:50 +0700

    25/03

--- javascript/43-XO.js
+++ javascript/43-XO.js
@@ -1,3 +1,7 @@
+/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+*/
+
 // function XO(str) {
 //     let arr =  str.toLowerCase().split('')
 //     return arr.filter(a => a != 'o'&'x').length == 0 ? arr.filter(a => a == 'x').length == arr.filter(a => a == 'o').length : true
@@ -10,8 +14,8 @@ XO = str => {
     return (x && x.length) === (o && o.length)  
 }
 
-console.log(XO("xo"));
-console.log(XO("xxoo"));
-console.log(XO("ooxxx"));
-console.log(XO("xxOo"));
-console.log(XO("bdvkzdvbiv"));
+console.log(XO("xo")); // true
+console.log(XO("xxoo")); // true
+console.log(XO("ooxxx")); // false
+console.log(XO("xxOo")); // true
+console.log(XO("bdvkzdvbiv")); // true
