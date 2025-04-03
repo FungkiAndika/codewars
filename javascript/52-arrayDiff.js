7334fbce21ff1494b0bb0d1a8ac587dfaa035667
FungkiAndika <fungki4444@gmail.com>
2025-04-03 21:47:11 +0700

    03/04

--- javascript/52-arrayDiff.js
+++ javascript/52-arrayDiff.js
@@ -1,12 +1,16 @@
-function arrayDiff(a, b) {
-    for (let i = 0; i < b.length; i++) {
-        const e = b[i];
-        if (i != b.length) {
-            a = a.filter(c => c != e)
-        }
-    }
-    return a
-}
+// function arrayDiff(a, b) {
+//     for (let i = 0; i < b.length; i++) {
+//         const e = b[i];
+//         if (i != b.length) {
+//             a = a.filter(c => c != e)
+//         }
+//     }
+//     return a
+// }
+
+// ALTER :
+const arrayDiff = (a,b) => a.filter(c => !b.includes(c))
+
 console.log(arrayDiff([1,2], [1])); // [2]
 console.log(arrayDiff([1,2,2], [1])); // [2, 2]
 console.log(arrayDiff([1,2,2], [2])); // [1]
