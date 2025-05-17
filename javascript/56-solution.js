5f63bb256b60f974989cfa0d3b0aa9f8a8289085
FungkiAndika <fungki4444@gmail.com>
2025-05-17 15:22:28 +0700

    revision

--- javascript/56-solution.js
+++ javascript/56-solution.js
@@ -1,10 +1,22 @@
 function solution(text, markers) {
-    const escapeRegExp = s => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    const regex = new RegExp(`[${markers.map(escapeRegExp).join("|")}]`, "g")
-    return !markers? text+" ": text.split("\n")
-    .map((line) => {
-        return markers.some(char => line.includes(char))?line.split(regex)[0]:line
-    }).join("\n")
+    // const escapeRegExp = s => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    // const regex = new RegExp(`[${markers.map(escapeRegExp).join("|")}]`, "g")
+    // return !markers? text+" ": text.split("\n")
+    // .map((line) => {
+    //     return markers.some(char => line.includes(char))?line.split(regex)[0]:line
+    // }).join("\n")
+
+    if (markers.length == 0) {
+        return text.trimEnd()
+    } else {
+        if (markers.length > 1) {
+            return text
+        } else {
+            return text.split('\n').map(a =>
+                a.indexOf(markers) >= 0 ? 
+                a.slice(0,a.indexOf(markers)-(a.length)).trimEnd() : a.trimEnd( )).join("\n")
+        }
+     }
 }
 
 const tests = [
@@ -38,4 +50,5 @@ const tests = [
   ];
 console.log(tests.map(a => solution(a[0],a[1]) === a[2]?"[*] ["+solution(a[0],a[1])+"]":"[x] ["+solution(a[0],a[1])+"] ["+a[2]+"]"));
 console.log("BENAR: "+tests.map(a => solution(a[0],a[1]) === a[2]).filter(a => a === true).length+", SALAH: "+tests.map(a => solution(a[0],a[1]) === a[2]).filter(a => a === false).length);
+
 
