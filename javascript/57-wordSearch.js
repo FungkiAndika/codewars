9a09252881c1028e0afc6f4b48c0479416ad3071
FungkiAndika <fungki4444@gmail.com>
2025-05-19 09:53:39 +0700

    19/05

---
+++ javascript/57-wordSearch.js
@@ -0,0 +1,13 @@
+function wordSearch(query, seq) {
+    s = seq.filter(a => /* RegExp(query.toUpperCase()).test(a.toUpperCase()) OR => */ a.toUpperCase().includes(query.toUpperCase()))
+    return s.length == 0 ? ["Empty"]:s
+}
+
+const test  = [
+    ["ab", ["za", "ab", "abc", "zab", "zbc"], ["ab", "abc", "zab"]],
+    ["aB", ["za", "ab", "abc", "zab", "zbc"], ["ab", "abc", "zab"]],
+    ["ab", ["za", "aB", "Abc", "zAB", "zbc"], ["aB", "Abc", "zAB"]],
+    ["abcd", ["za", "aB", "Abc", "zAB", "zbc"], ["Empty"]]
+
+]
+console.log(test.map(a => wordSearch(a[0],a[1])))
