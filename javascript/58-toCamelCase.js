abeaa35473b9726ce950832ceef6f74d1d55aa4a
FungkiAndika <fungki4444@gmail.com>
2025-05-19 10:44:24 +0700

    19/05

--- javascript/58-toCamelCase.js
+++ javascript/58-toCamelCase.js
@@ -1,7 +1,12 @@
 function toCamelCase(str) {
-    return str
+    const regex = RegExp(/[_-]/) 
+    return  str.split(regex)[0]+str.split(regex).map( a => a.charAt(0).toUpperCase()+a.slice(1)).slice(1).join("")
 }
 
 test = [
-    
+    ['', ''],
+    ["the_stealth_warrior", "theStealthWarrior"],
+    ["The-Stealth-Warrior", "TheStealthWarrior"],
+    ["A-B-C", "ABC"]
 ]
+console.log(test.map(a => toCamelCase(a[0])));
